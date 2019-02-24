c2720798b261716513b0315c203d8cf34ee07c3c
Robin <1105080+openscript@users.noreply.github.com>
2019-02-24 14:33:50 +0100

    Enhance scraper

--- scraper.rb
+++ scraper.rb
@@ -2,6 +2,7 @@ require 'httparty'
 require 'nokogiri'
 require 'active_support/all'
 
+save_options = {encoding: "UTF-8", indent: 4, save_with: Nokogiri::XML::Node::SaveOptions::FORMAT | Nokogiri::XML::Node::SaveOptions::AS_XML}
 output_directory = './out/'.freeze
 languages = %w(de it fr)
 
@@ -67,20 +68,24 @@ module_list.each do |m|
               xml.goal {
                 xml.text("\n#{' '*12}")
                 xml.text(t.css('> td:nth-child(2)').first.text)
-                xml.text("\n#{' '*12}")
-                xml.send('knowledge-list') {
-                  xml.text("\n#{' '*16}")
-                  knowledge = module_table_data[i+1].css('table > tr')
-                  knowledge.each_with_index do |tt, ii|
-                    xml.knowledge "\n#{' '*20}" + tt.css('> td:nth-child(2)').text + "\n#{' '*16}"
-                    if ii == knowledge.length - 1
-                      xml.text("\n#{' ' * 12}")
-                    else
+                knowledge = module_table_data[i+1].css('table > tr')
+                if knowledge.empty?
+                  puts "No knowledge points defined in #{m[module_number_attribute]}!"
+                else
+                  xml.text("\n#{' '*12}")
+                  xml.send('knowledge-list') {
+                    knowledge.each_with_index do |tt, ii|
+                      knowledge_text = tt.css('> td:nth-child(2)').text
                       xml.text("\n#{' ' * 16}")
+                      xml.knowledge "\n#{' ' * 20}" + knowledge_text + "\n#{' ' * 16}"
+
+                      if ii == knowledge.length - 1
+                        xml.text("\n#{' ' * 12}")
+                      end
                     end
-                  end
-                }
-                xml.text("\n#{' '*8}")
+                  }
+                  xml.text("\n#{' ' * 8}")
+                end
               }
             end
           end
@@ -93,11 +98,11 @@ module_list.each do |m|
     end
 
     File.open(output_directory + dirname + "/#{l}-ch.xml", 'w') do |file|
-      file.write module_builder.to_xml(encoding: "UTF-8", indent: 4)
+      file.write module_builder.to_xml(save_options)
     end
   end
   module_builder = Nokogiri::XML::Builder.new do |xml|
-    xml.competency('xmlns' => 'https://ictorg.ch/competency', 'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation' => 'https://ictorg.ch/competency ../../../schema/competency.xsd') {
+    xml.competency('xmlns' => 'https://ictorg.ch/competency', 'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation' => 'https://ictorg.ch/competency ../../schema/competency.xsd') {
       module_languages.each do |l|
         xml.description('xml:lang' => "#{l}-ch") {
           xml.text("./#{dirname}/#{l}-ch.xml")
@@ -106,7 +111,7 @@ module_list.each do |m|
     }
   end
   File.open(output_directory + "/#{dirname}.xml", 'w') do |file|
-    file.write module_builder.to_xml(encoding: "UTF-8", indent: 4)
+    file.write module_builder.to_xml(save_options)
   end
   puts "Scraped module #{m[module_number_attribute]}."
 end
